2198eaf9704f8e7ac58351d3b7004998742fcd6f
ABDULDEV <abdulkadiradamu203@gmail.com>
2024-10-17 16:06:19 +0100

    feat:SOME NEW FEATURES

--- JS/index.js
+++ JS/index.js
@@ -1,9 +1,18 @@
 
+// let username = prompt("WHAT IS YOUR NAME?");
+
+// function greet(username){
+//   return "HELLO, " + username;
+// }
+
+// window.alert = (greet(username));
+
+
 // script.js
 document.getElementById('btn').addEventListener('click', function() {
     var btn = document.getElementById('services');
     
-    // Toggle the display of the anchor list
+  
     if (services.style.display === 'none' || services.style.display === '') {
       services.style.display = 'block';
       this.textContent = 'Hide Links'; // Change button text
@@ -12,6 +21,31 @@ document.getElementById('btn').addEventListener('click', function() {
       this.textContent = 'SERVICES'; // Change button text
     }
   });
-var name = musa 
+  
+   // Array of image sources
+   const images = ['./image/pic1.jpeg', './image/pic3.jpg'];
 
- console.log('welcome' + name)
+   let currentIndex = 0;
+
+   // Function to change the image
+   function changeImage() {
+       currentIndex = (currentIndex + 1) % images.length; // Move to next image, loop back if at end
+       document.getElementById('image-slider').src = images[currentIndex]; // Update the image source
+   }
+
+   // Automatically change the image every 10 seconds (10000 milliseconds)
+   setInterval(changeImage, 5000);
+
+  
+    // Function to adjust layout based on window size
+    function adjustLayout() {
+      const content = document.getElementById('container');
+
+      if (window.innerWidth <= 767) {
+          // For small screens (mobile), stack boxes vertically
+          content.style.flexDirection = 'row';
+      } else {
+          // For larger screens (tablet and desktop), display boxes in a row
+          content.style.flexDirection = 'row'; 
+      }
+  }
